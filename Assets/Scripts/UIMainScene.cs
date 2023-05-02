11c195a80d4cc749e9f54bc836db25290766ec15
FranciscoCabrerizo <fdmcabrerizo@gmail.com>
2023-05-02 12:12:15 +0200

    Add Start Menu and buttons to move between scenes
    New scene (Start menu) has been added and Start and Quit buttons have been implemented. In main scene, a 'Back to menu' button has alse been added.

---
+++ Assets/Scripts/UIMainScene.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMainScene : MonoBehaviour
+{
+    public void BackToMenu() {
+        SceneManager.LoadScene(0);
+    }
+}
